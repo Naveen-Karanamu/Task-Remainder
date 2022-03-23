43978acc8df21c9e4287c11967f02ec0607d68a8
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-23 18:32:36 +0530

    Added: Task conditions

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ import time
 name=input("Hello user! please enter you name 🙃 ")
 print ("*"*20,"WELCOME TO THE TASK REMAINDER 😌","*"*20)
 
-task=input(f"{name} please enter the task that you should be remainded off!")
-repeats=int(input("Please Enter the number of times you want us to remaind your task"))
+task=input(f"{name} please enter the task that you should be remainded off! ")
 
-print("Tell us the unit of time after which you want to be remainded about the task")
-print("Type 'S' for seconds, 'M' for minutes and 'H' for hours")
+def repeats_func():
+    repeats=input("Please Enter the number of times you want us to remaind your task ")
+    if(repeats.isnumeric()):
+        return repeats
+    else:
+        print("Invalid input ⚠️ Please Enter a valid input!")
+        repeats_func()
+    
+times=int(repeats_func())
+
+print("Tell us the unit of time after which you want to be remainded about the task ")
+t=input("Type 'S' for seconds, 'M' for minutes and 'H' for hours ")
+
+
+
+while (times>0):
+    time.sleep(3)
+    print(f"{name} !!!, it's time to {task}")
+    times-=1
