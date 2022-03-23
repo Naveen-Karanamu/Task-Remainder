d9f534f5674f8fdded9933a3c89eee6a42a75576
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-03-23 18:12:45 +0530

    Initialized: Project with welcomes

---
+++ main.py
@@ -0,0 +1,12 @@
+# Modules
+import time
+
+# Welcome
+name=input("Hello user! please enter you name 🙃 ")
+print ("*"*20,"WELCOME TO THE TASK REMAINDER 😌","*"*20)
+
+task=input(f"{name} please enter the task that you should be remainded off!")
+repeats=int(input("Please Enter the number of times you want us to remaind your task"))
+
+print("Tell us the unit of time after which you want to be remainded about the task")
+print("Type 'S' for seconds, 'M' for minutes and 'H' for hours")
